fc18cb45f92a3465af03358ab9167072c0b6c84a
scy_zxj <zhangxianjiang887@126.com>
2017-12-17 23:30:44 +0800

    增加输出日志

--- src/com/hollow/common/Test.java
+++ src/com/hollow/common/Test.java
@@ -2,9 +2,12 @@ package com.hollow.common;
 
 public class Test {
 
+	public static void printLog(String str){
+		System.out.println("=====:"+str);
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("-------");
 	}
 
 }
